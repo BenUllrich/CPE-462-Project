d1d0da39bdc8588ccf5e1b93b5ecd3357dc94d69
alan-m12 <63511002+alan-m12@users.noreply.github.com>
2024-05-11 17:21:42 -0400

    Update and rename wavelettransform.m to wavelettransformCompression.m

---
+++ wavelettransformCompression.m
@@ -0,0 +1,24 @@
+clear all;
+close all;
+clc;
+
+A = imread('C:\Users\alanm\Downloads\birds.jpg');
+B = rgb2gray(A);
+
+%% Wavelet Compression
+[C,S] = wavedec2(B, 4, 'db1');
+Coeff_sort =sort(abs(C(:)));
+
+count = 1;
+for keep = [.1 .05 .01 .005]
+    subplot(2,2,count)
+    thresh = Coeff_sort(floor((1-keep)*length(Coeff_sort)));
+    index = abs(C)>thresh;
+    C_filter = C.*index;
+
+    % Reconstruction
+    Areacon = uint8(waverec2(C_filter, S, 'db1'));
+    imshow(uint8(Areacon))
+    title(['', num2str(keep*100),'%'], 'FontSize',12)
+    count = count+1;
+end
