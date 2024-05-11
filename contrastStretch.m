c1d982825f1c16edac239d42e26665163ff07e52
alan-m12 <63511002+alan-m12@users.noreply.github.com>
2024-05-11 16:10:40 -0400

    Update contrastStretch.m

--- contrastStretch.m
+++ contrastStretch.m
@@ -1,26 +1,44 @@
-% Read the image
-img = imread(%input file path here%);
-
-% Convert to double for calculations
-img_double = double(img) / 255; % Normalize to [0, 1]
-
-% Find the minimum and maximum pixel intensities
-min_intensity = min(img_double(:));
-max_intensity = max(img_double(:));
-
-% Perform contrast stretching
-output_img = (img_double - min_intensity) / (max_intensity - min_intensity) * 255;
-
-% Convert the output image to uint8 for display
-output_img_uint8 = uint8(output_img);
-
-% Display the original and stretched images
-figure;
-subplot(1,2,1);
-imshow(img);
-title('Original Image');
-subplot(1,2,2);
-imshow(output_img_uint8);
-title('Contrast Stretched Image');
-
-% Save the output image
+clear all;
+close all;
+clc;
+i=imread('C:\Users\alanm\Downloads\bean.jpg');
+s=size(i);
+r1=input('Enter r1: ');
+r2=input('Enter r2: ');
+if (r1>=0 && r1<=s(1)-1 && r2>=0 && r2<=s(1)-1 && r1<r2)
+    alpha_degree=input('Enter alpha in degrees: ');
+    beta_degree=input('Enter beta in degrees: ');
+    gamma_degree=input('Enter gamma in degrees: ');
+    alpha=(alpha_degree*pi)/180;
+    beta=(beta_degree*pi)/180;
+    gamma=(gamma_degree*pi)/180;
+    % range definitions
+    x_r1 = 0:r1;
+    x_r2 = r1:r2;
+    x_r3 = r2:s(1)-1;
+    % line gradients
+    a1 = tan(alpha);
+    a2 = tan(beta);
+    a3 = tan(gamma);
+    % line functions
+    yo_1 = floor(a1*x_r1);
+    yo_2 = floor(a2*x_r2+(a1-a2)*r1);
+    yo_3 = floor(a3*x_r3+r2*(a2-a3)+r1*(a1-a2));
+    % line concatenation
+    y = [yo_1 yo_2 yo_3];
+    figure,plot(y);
+    xlim([0 s(1)-1]);
+    ylim([0 s(2)-1]);
+    % contrast stretching in regions
+    im1 = floor(a1*i);
+    im2 = floor(a1*r1 + (a2*(i-r1)));
+    im3 = floor(a2*r2+(a1-a2)*r1 + (a3*(i-r2)));
+    % concatance of output image
+	imageOut = cast(im1+im2+im3,'uint8');
+    figure,imshow(i)
+    title('Original Image');
+	figure,imshow(imageOut);
+    title('Contrast Stretching');
+else 
+    display('Invalid values of r1 and r2');
+end
